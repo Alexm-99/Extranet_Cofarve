4aa8fa725ada9f9f9a2aae593b205361dac6835f
alexmedranda <alexmedranda_99@hotmail.com>
2022-04-20 15:53:18 -0500

    cambio 20-04-2022

---
+++ Extranet_Cofarve/blog/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+
+# Create your models here.
+
+
+class link(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    icon = models.CharField(max_length=300)
+    state = models.BooleanField()
+
+    def __str__(self):
+        return self.id
+
+
+class Galeria(models.Model):
+    id = models.AutoField(primary_key=True)
+    imageX = models.FileField(upload_to='public/static/', max_length=254, blank=True)
+   
+
+    def __str__(self):
+        return self.id
+
+
+class RedeSociales(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    state = models.BooleanField()
+    enlace = models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return self.id
+
+
+class TemasImportantes(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    enlace = models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return self.id
+
+class Noticias(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    enlace = models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return self.id
+
+class administrador(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.CharField(max_length=100)
+    email = models.EmailField()
+    passwd = models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return self.id
+
---
+++ Extranet_Cofarve/public/static/js/admin.js
@@ -0,0 +1,15 @@
+function principal(){
+    cuerpo = document.getElementById("cuerpo");
+    let addHtml = ``;
+
+    
+
+    addHtml +=` <h1> Hola Mundo </h1> `
+
+
+    cuerpo.innerHTML = addHtml;
+
+
+}
+
+principal();
