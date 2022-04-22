4f04e140b332ac91d4aef8c86fb1b1a09d5578a1
alexmedranda <alexmedranda_99@hotmail.com>
2022-04-22 12:17:29 -0500

    cambios 22-04-2022

--- Extranet_Cofarve/blog/views.py
+++ Extranet_Cofarve/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.db import connection
-from .models import link
+from .models import link, linkSecond
 
 # Create your views here.
 def inicio(request):
     enlace = link.objects.all()
-    contexto = {'link':enlace}
+    enlace2 = linkSecond.objects.all()
+    contexto = {'link':enlace , 'link2':enlace2}
     return render(request, 'index.html', contexto)
